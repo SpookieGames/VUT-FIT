cabdbe621d523b471324c2df466d364e893b7048
Michal Holeša <misko.holesa@gmail.com>
2025-11-30 12:38:08 +0100

    Refactor flow structure: adjust IP address field sizes, enhance error handling in load_file function, and add IP verification

--- IZP/Projekt 2/flows.c
+++ IZP/Projekt 2/flows.c
@@ -6,8 +6,8 @@
 typedef struct Sflow
 {
     int ID; // flow ID
-    char src_ip[20];
-    char dst_ip[20];
+    char src_ip[16];
+    char dst_ip[16];
     int b;   // total_bytes
     int t;   // flow_duration
     float d; // avg_interarrival_time
@@ -27,7 +27,7 @@ typedef struct Sweights
 
 // Deklaracia funkcii
 void load_weights(char *argv[], weights *W);
-flow *load_file(const char *filename, int *count_out);
+flow *load_file(char *filename, int *count_out);
 int verify_arguments(int argc, char *argv[], weights *W);
 
 void load_weights(char *argv[], weights *W)
@@ -38,11 +38,11 @@ void load_weights(char *argv[], weights *W)
     W->ws = atof(argv[6]); //
 }
 
-flow *load_file(const char *filename, int *count_out)
+flow *load_file(char *filename, int *count_out)
 {
     int count, flow_id, flow_duration, total_bytes, packet_count;
-    char src_ip[20];
-    char dst_ip[20];
+    char src_ip[16];
+    char dst_ip[16];
     float avg_interarrival;
 
     FILE *f = fopen(filename, "r"); // Otvorenie suboru na citanie
@@ -58,7 +58,7 @@ flow *load_file(const char *filename, int *count_out)
         return NULL;
     }
 
-    flow *flow_array = malloc(count * sizeof(flow)); // Alokovanie pamata pre vsetky flow
+    flow *flow_array = malloc(count * sizeof(flow)); // Alokovanie pamate pre vsetky flow
     if (flow_array == NULL)
     {
         fprintf(stderr, "Failed to allocate memory\n");
@@ -69,15 +69,15 @@ flow *load_file(const char *filename, int *count_out)
     // Cyklus na populovanie flow_array
     for (int i = 0; i < count; i++)
     {
-        if (fscanf(f, "%d %s %s %d %d %d %f", &flow_id, src_ip, dst_ip, &total_bytes, &flow_duration, &packet_count, &avg_interarrival) != 7)
+        if (fscanf(f, "%d %15s %15s %d %d %d %f", &flow_id, src_ip, dst_ip, &total_bytes, &flow_duration, &packet_count, &avg_interarrival) != 7)
         {
-            fprintf(stderr, "Count is incorrect\n");
+            fprintf(stderr, "Count is incorrect or %s has invalid data\n", filename);
             fclose(f);
             free(flow_array);
             return NULL;
         }
 
-        flow_array[i].ID = flow_id;           // Odovzdanie nacitanych hodnot do flow
+        flow_array[i].ID = flow_id;           // Odovzdanie nacitanych hodnot do flow_array
         strcpy(flow_array[i].src_ip, src_ip); //
         strcpy(flow_array[i].dst_ip, dst_ip); //
         flow_array[i].b = total_bytes;        //
@@ -86,7 +86,7 @@ flow *load_file(const char *filename, int *count_out)
 
         if (packet_count <= 0) // Neda sa delit nulou a zaporny pocet paketov nedava zmysel
         {
-            fprintf(stderr, "Packet count cannot be 0");
+            fprintf(stderr, "Packet count cannot be 0 or negative");
             fclose(f);
             free(flow_array);
             return NULL;
@@ -104,10 +104,42 @@ flow *load_file(const char *filename, int *count_out)
     return flow_array;
 }
 
+// Funkcia na overenie IP adries. IP nesmie mat cislo vacsie ako 255 a mensie ako 0 medzi bodkami. Pouzil som sscanf co je posobny ako fscanf ale namiesto suboru cita z textoveho retazca
+int verify_ips(int count, flow *flow_array)
+{
+    int as, bs, cs, ds;
+    int ad, bd, cd, dd;
+    for (int i = 0; i < count; i++)
+    {
+        if (sscanf(flow_array[i].src_ip, "%d.%d.%d.%d", &as, &bs, &cs, &ds) != 4)
+        {
+            fprintf(stderr, "Invalid IP address\n");
+            return 1;
+        }
+        else if (as < 0 || as > 255 || bs < 0 || bs > 255 || cs < 0 || cs > 255 | ds < 0 || ds > 255)
+        {
+            fprintf(stderr, "Invalid IP address\n");
+            return 1;
+        }
+
+        if (sscanf(flow_array[i].dst_ip, "%d.%d.%d.%d", &ad, &bd, &cd, &dd) != 4)
+        {
+            fprintf(stderr, "Invalid IP address\n");
+            return 1;
+        }
+        else if (ad < 0 || ad > 255 || bd < 0 || bd > 255 || cd < 0 || cd > 255 | dd < 0 || dd > 255)
+        {
+            fprintf(stderr, "Invalid IP address\n");
+            return 1;
+        }
+    }
+    return 0;
+}
+
 // Funkcia na verifikaciu platnosti zadanych argumentov
 int verify_arguments(int argc, char *argv[], weights *W)
 {
-    char *p_end; // Potrebujeme na verifikovanie premien strtol a strtod
+    char *p_end; // Potrebujeme na verifikovanie premien strtol a strtod (ukazovatel na koniec) podla dokumentacie
     if (argc != 7)
     {
         fprintf(stderr, "Enter correct number of arguments!\n");
@@ -138,16 +170,25 @@ int main(int argc, char *argv[])
     {
         return 1;
     }
-    const char *filename = argv[1];
-    const int n = atoi(argv[2]);
-    flow *flow_array = load_file(filename, &count); // Vytvorime pole flows
+    char *filename = argv[1];
+    int n = atoi(argv[2]);
+    flow *flow_array = load_file(filename, &count); // Vytvorime pole pre flows
 
     if (flow_array == NULL)
     {
         return 1;
     }
 
+    if (verify_ips(count, flow_array) != 0)
+    {
+        return 1;
+    }
     // temporary
+    for (int i = 0; i < count; i++)
+    {
+        printf("ID: %d\n", flow_array[i].ID);
+    }
+
     // printf("%f %f %f %f\n", W.wb, W.wt, W.wd, W.ws);
     printf("Count: %d\n", count);
 
