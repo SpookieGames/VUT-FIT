08851f39099cc1a1c3de2bbab22cfa3da41ac7ad
Michal Holeša <misko.holesa@gmail.com>
2026-03-02 16:16:17 +0100

    Remove placeholder comments and add structure to black box tests for Red-Black Tree

--- IVS/Projekt 1/black_box_tests.cpp
+++ IVS/Projekt 1/black_box_tests.cpp
@@ -20,19 +20,6 @@
 #include "red_black_tree.h"
 
 //============================================================================//
-// ** ZDE DOPLNTE TESTY **
-//
-// Zde doplnte testy Red-Black Tree, testujte nasledujici:
-// 1. Verejne rozhrani stromu
-//    - InsertNode/DeleteNode a FindNode
-//    - Chovani techto metod testuje pro prazdny i neprazdny strom.
-// 2. Axiomy (tedy vzdy platne vlastnosti) Red-Black Tree:
-//    - Vsechny listove uzly stromu jsou *VZDY* cerne.
-//    - Kazdy cerveny uzel muze mit *POUZE* cerne potomky.
-//    - Vsechny cesty od kazdeho listoveho uzlu ke koreni stromu obsahuji
-//      *STEJNY* pocet cernych uzlu.
-//============================================================================//
-
 class EmptyTree : public ::testing::Test
 {
 protected:
@@ -189,5 +176,6 @@ TEST_F(TreeAxioms, Axiom3)
         }
     }
 }
+//============================================================================//
 
 /*** Konec souboru black_box_tests.cpp ***/
