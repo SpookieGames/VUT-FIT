e51d63f60eb443795541f46fd877691c72b35f9b
Michal Holeša <misko.holesa@gmail.com>
2026-03-03 21:24:59 +0100

    update cmake

--- build/black_box_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/mnt/c/Users/Spookie/Documents/VS Code/VUT FIT/build/black_box_test[1]_tests.cmake")
-  include("/mnt/c/Users/Spookie/Documents/VS Code/VUT FIT/build/black_box_test[1]_tests.cmake")
-else()
-  add_test(black_box_test_NOT_BUILT black_box_test_NOT_BUILT)
-endif()
